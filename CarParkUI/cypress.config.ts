1c1bb18cf4707ee222c4ad25976dc39e9659c231
LackosS <84352399+LackosS@users.noreply.github.com>
2023-03-22 23:13:12 +0100

    cypress fix

--- CarParkUI/cypress.config.ts
+++ CarParkUI/cypress.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     experimentalRunAllSpecs: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 10000,
+    screenshotOnRunFailure: false,
+    videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
     },
   },
